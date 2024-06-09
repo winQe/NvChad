a6fa87c28cc97d0c971b6355ed349258151175a4
Andhika Satriya Bhayangkara <85143700+winQe@users.noreply.github.com>
2024-06-09 18:30:55 +0800

    Added more snippets

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -98,6 +98,14 @@ return {
           require("luasnip").config.set_config(opts)
           require "nvchad.configs.luasnip"
           require("vim-react-snippets").lazy_load()
+          require("luasnip").filetype_extend("typescript", { "tsdoc" })
+          require("luasnip").filetype_extend("javascript", { "jsdoc" })
+          require("luasnip").filetype_extend("lua", { "luadoc" })
+          require("luasnip").filetype_extend("python", { "pydoc" })
+          require("luasnip").filetype_extend("rust", { "rustdoc" })
+          require("luasnip").filetype_extend("c", { "cdoc" })
+          require("luasnip").filetype_extend("cpp", { "cppdoc" })
+          require("luasnip").filetype_extend("sh", { "shelldoc" })
         end,
       },
 
