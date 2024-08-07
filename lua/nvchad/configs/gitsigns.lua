ae102b7808e367795edde1bfac93c4c0e732707c
Adin <andhika.bhayangkara@desaysv.com>
2024-08-07 14:10:25 +0800

    Updated gitsigns config

--- lua/nvchad/configs/gitsigns.lua
+++ lua/nvchad/configs/gitsigns.lua
@@ -13,15 +13,48 @@ local options = {
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
-    local function opts(desc)
-      return { buffer = bufnr, desc = desc }
-    end
-
     local map = vim.keymap.set
 
-    map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
-    map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
-    map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return "<Ignore>"
+    end, { expr = true, desc = "Next Git Hunk" })
+
+    map("n", "[c", function()
+      if vim.wo.diff then
+        return "[c"
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return "<Ignore>"
+    end, { expr = true, desc = "Previous Git Hunk" })
+
+    -- Actions
+    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Git Hunk" })
+    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Git Hunk" })
+    map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
+    map("n", "<leader>ha", gs.stage_hunk, { desc = "Stage Hunk" })
+    map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+    map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
+    map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+    map("n", "<leader>hb", function()
+      gs.blame_line { full = true }
+    end, { desc = "Blame Line" })
+    map("n", "<leader>tB", gs.toggle_current_line_blame, { desc = "Toggle Current Line Blame" })
+    map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
+    map("n", "<leader>hD", function()
+      gs.diffthis "~"
+    end, { desc = "Diff This ~" })
+
+    -- Text object
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
   end,
 }
 
