40151022bfcd0bcfa84c494d41284b9965607f49
Adin <andhika.bhayangkara@desaysv.com>
2024-08-08 13:41:04 +0800

    Updated gitsigns desc

--- lua/nvchad/configs/gitsigns.lua
+++ lua/nvchad/configs/gitsigns.lua
@@ -39,22 +39,22 @@ local options = {
     -- Actions
     map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Git Hunk" })
     map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Git Hunk" })
-    map("n", "<leader>hS", gs.stage_buffer, { desc = "Stage Buffer" })
-    map("n", "<leader>ha", gs.stage_hunk, { desc = "Stage Hunk" })
-    map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-    map("n", "<leader>hR", gs.reset_buffer, { desc = "Reset Buffer" })
-    map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+    map("n", "<leader>hS", gs.stage_buffer, { desc = "Git Stage Buffer" })
+    map("n", "<leader>ha", gs.stage_hunk, { desc = "Git Stage Hunk" })
+    map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Git Undo Stage Hunk" })
+    map("n", "<leader>hR", gs.reset_buffer, { desc = "Git Reset Buffer" })
+    map("n", "<leader>hp", gs.preview_hunk, { desc = "Git Preview Hunk" })
     map("n", "<leader>hb", function()
       gs.blame_line { full = true }
     end, { desc = "Blame Line" })
-    map("n", "<leader>tB", gs.toggle_current_line_blame, { desc = "Toggle Current Line Blame" })
-    map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
+    map("n", "<leader>tB", gs.toggle_current_line_blame, { desc = "Git Toggle Current Line Blame" })
+    map("n", "<leader>hd", gs.diffthis, { desc = "Git Diff This" })
     map("n", "<leader>hD", function()
       gs.diffthis "~"
-    end, { desc = "Diff This ~" })
+    end, { desc = "Git Diff This ~" })
 
     -- Text object
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Git Select Hunk" })
   end,
 }
 
